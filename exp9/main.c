5ee4ca21a9148fb0b664a996c1dd0ea97b7a00fe
V6429 <cb.en.p2ebs21015@cb.students.amrita.edu>
2022-04-01 11:49:57 +0530

    pll func

--- exp9/main.c
+++ exp9/main.c
@@ -4,19 +4,23 @@
 #include <string.h>
 #include <stdlib.h>//atoi alternative 
 #define PRINTBUFFER 0 // set 1 or 0
+#define APBDIVVAL 0x00  // 00 div by 4  ,01 no div 
 
 // what to do on uart0
 //**********************************************************************
 // use the initfunction
 // use the sendstring function
 // recived charachters are loaded in a buffer through interrupt
+void set_pll(void);
 int main()
 {
     int i = 0;
     char transmitstring1[] = "Hello world\n";
     char tempstringbffr[20];
     char tempstr[10];
+	set_pll();
 	BUFFER[0]='\0';		 // an extern buffer
+
 
     IO1DIR = (1 << LED1) | (1 << LED2) | (1 << LED3) | (1 << LED4);
 
@@ -75,9 +79,38 @@ int main()
                 BUFFER[BUFRTAIL]='\0';
                 uartSendString("LPC2148: WORLD 	(ノ ゜Д゜)ノ ︵ ┻━┻\n");
             }
+			  if(strstr(BUFFER,"buffer")!=NULL){
+		uartSendString("BUFFER=");
+		replaceAll(BUFFER,'\n','#');        
+        uartSendString(BUFFER); // prints the entire buffer
+		uartSendString("\n");
+		memset(BUFFER,'0',100);	// clear buffer
+        BUFRTAIL=0;
+		BUFFER[BUFRTAIL]='\0';
+        
+
+         // for neet display
+
+
+            }
+
         
         }
     }
 
     return 0;
+}
+
+
+void set_pll(void)
+{
+  PLL0CON=0x01;             // PPLE=1 & PPLC=0 so it will be enabled  but not connected after FEED sequence
+  PLL0CFG=0x24;          // set the multipler to 5 (i.e actually 4) i.e 12x5 = 60 Mhz (M - 1 = 4), Set P=2 since we want FCCO in range So , Assign PSEL =01 in PLL0CFG as per the table.
+  PLL0FEED=0XAA;          //feed
+  PLL0FEED=0X55;
+  while((PLL0STAT&(1<<10))==0);      // check whether PLL has locked on to the  desired freq by reading the lock bit in the PPL0STAT register
+  PLL0CON=0x03;          //enable & connect pll
+  PLL0FEED=0XAA;
+  PLL0FEED=0X55;
+  VPBDIV = APBDIVVAL;          // PCLK is same as CCLK i.e 60Mhz	when 01
 }
